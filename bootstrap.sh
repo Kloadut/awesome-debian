8159ecea5a4cf9019f3d3758cee5663895473e87
Kload <kload@kload.fr>
2012-09-22 14:14:29 +0200

    Init

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,9 +18,15 @@ for i in $HOME/.config/awesome $endpath; do [ -e $i ] && mv $i $i.$today; done
 echo "cloning kload-awesome\n"
 git clone --recursive http://github.com/Kloadut/awesome-debian.git $endpath
 
-sed -i "s/#HOME#/$HOME/g" $endpath/rc.lua
-sed -i "s/#HOME#/$HOME/g" $endpath/themes/skymod/theme.lua
+sed -i s/\#HOME\#/$(echo $HOME | sed 's/\//\\\//g')/g $endpath/rc.lua
+sed -i s/\#HOME\#/$(echo $HOME | sed 's/\//\\\//g')/g $endpath/themes/skymod/theme.lua
 
 ln -s $endpath $HOME/.config/awesome
+
+echo "copying Robot font\n"
+cp $endpath/fonts/* $HOME/.fonts/
+
+echo "clearing font cache\n"
+fc-cache -vf
 
 echo "Done\n"
